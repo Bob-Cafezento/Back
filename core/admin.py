22262958d145064f7bd493cd32e33642dc852f62
pjgplantas <pjgplantas@gmail.com>
2022-09-15 14:36:21 -0300

    Basico do Models e Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Resposta, Pergunta, Formulario, Sel_res_form, Responde
+
+admin.site.register(Resposta)
+admin.site.register(Pergunta)
+admin.site.register(Formulario)
+admin.site.register(Sel_res_form)
+admin.site.register(Responde)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Resposta(models.Model):
+    textpresp = models.CharField(max_length=100)
+
+    def __str__(self):
+            return self.textpresp
+
+class Pergunta(models.Model):
+    alternativas = models.CharField(max_length=100)
+    texto_per = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.alternativas
+
+
+class Formulario(models.Model):
+    titulo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.titulo
+
+class Sel_res_form(models.Model):
+    odth = models.DateField()
+    cod_form = models.IntegerField()
+
+    def __str__(self):
+        return self.odth
+
+class Responde(models.Model):
+    cod_form = models.IntegerField()
+    dth = models.DateField()
+
+    def __str__(self):
+        return self.cod_form
