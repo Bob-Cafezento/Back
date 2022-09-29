3a4837250be2097548f7dd7faf4d907ae596dc15
Bob-Cafezento <successstudent9@gmail.com>
2022-09-29 16:08:33 -0300

    aula 29/09/22

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from core.models import Resposta, Pergunta, Formulario, Sel_res_form, Responde
+from core.models import Resposta, Pergunta, Formulario, Sel_res_form, Responde, Conteudo, Midia
 
 admin.site.register(Resposta)
 admin.site.register(Pergunta)
 admin.site.register(Formulario)
 admin.site.register(Sel_res_form)
 admin.site.register(Responde)
+admin.site.register(Conteudo)
+admin.site.register(Midia)
--- core/models.py
+++ core/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 
 class Resposta(models.Model):
-    textpresp = models.CharField(max_length=100)
+    cod_resp = models.IntegerField()
+    status = models.CharField(max_length=100)
+    textpresp = models.CharField(max_length=250)
 
     def __str__(self):
-            return self.textpresp
+            return self.cod_resp
 
 class Pergunta(models.Model):
-    alternativas = models.CharField(max_length=100)
-    texto_per = models.CharField(max_length=100)
+    cod_p = models.IntegerField()
+    gabarito = models.CharField(max_length=5)
+    alternativas = models.CharField(max_length=250)
+    texto_per = models.TextField(max_length=500)
 
     def __str__(self):
-        return self.alternativas
+        return self.cod_p
 
 
 class Formulario(models.Model):
+    cod_form = models.IntegerField()
     titulo = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.titulo
+        return self.cod_form
 
 class Sel_res_form(models.Model):
-    odth = models.DateField()
     cod_form = models.IntegerField()
+    odth = models.DateField()
 
     def __str__(self):
-        return self.odth
-
+        return self.cod_form
 
 class Responde(models.Model):
     cod_form = models.IntegerField()
@@ -34,3 +38,22 @@ class Responde(models.Model):
 
     def __str__(self):
         return self.cod_form
+    
+class Conteudo(models.Model):
+    id_conteudo = models.IntegerField()
+    titulo = models.CharField(max_length=200)
+    paragrafo = models.TextField(max_length=2000)
+    topico = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.id_conteudo
+
+class Midia(models.Model):
+    cod_midia = models.IntegerField()
+    imagens = models.ImageField()
+    audios = models.FileField()
+    videos = models.FileField()
+    
+    def __str__(self):
+        return self.cod_midia
+    
